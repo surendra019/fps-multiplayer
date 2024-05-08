c80066c6ad0557ce179a12c6287afd26e9bf702b
surendra019 <iamsurendra019@gmail.com>
2024-05-08 21:28:31 +0530

    ads

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,9 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+var SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+
 
 
 @export var joystick : Node2D
@@ -10,12 +11,17 @@ const JUMP_VELOCITY = 4.5
 @export var jump_btn: Node
 @export var crosshair: Node
 @export var fire_btn: Node
+@export var ads_btn: Node
 
 @onready var pistol = $CameraPivot/GunPivot/Pistol
 @onready var animation_player = $AnimationPlayer
 @onready var camera_3d = $CameraPivot/Camera3D
+@onready var ads_pivot = $CameraPivot/AdsPivot
+@onready var ads_camera = $CameraPivot/AdsCamera
+
 
 var CAMERA_INIT_POSITION: Vector3
+var enable_ads: bool
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -37,8 +43,20 @@ func handle_inputs(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
-
+	if Input.is_action_just_pressed("ads"):
+		toggle_ads()
 	
+	if enable_ads:
+		crosshair.hide()
+		ads_camera.current = true
+		ads_camera.global_position = lerp(ads_camera.global_position, ads_pivot.global_position, .2)
+		SPEED = 2.0
+	else:
+		crosshair.show()
+		ads_camera.global_position = lerp(ads_camera.global_position, camera_3d.global_position, .2)
+		if ads_camera.global_position.is_equal_approx(camera_3d.global_position):
+			camera_3d.current = true
+		SPEED = 5.0
 		
 	var input_dir
 	if OS.get_name() == "Android":
@@ -72,7 +90,7 @@ func handle_inputs(delta):
 	move_and_slide()
 func _on_camera_drag_gui_input(event):
 	if event is InputEventScreenDrag:
-		self.rotation.y -= event.relative.x/1000*Manager.CAMERA_SENSITIVITY
+		self.rotation.y = event.relative.x/1000*Manager.CAMERA_SENSITIVITY
 
 
 func set_platform_configurations():
@@ -81,11 +99,13 @@ func set_platform_configurations():
 		camera_drag.show()
 		jump_btn.show()
 		fire_btn.show()
+		ads_btn.show()
 	else:
 		joystick.hide()
 		camera_drag.hide()
 		jump_btn.hide()
 		fire_btn.hide()
+		ads_btn.hide()
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
@@ -102,3 +122,11 @@ func _on_jump_pressed():
 
 func _on_fire_pressed():
 	pistol.fire_bullet()
+
+func toggle_ads():
+	enable_ads = !enable_ads
+	
+
+
+func _on_ads_pressed():
+	toggle_ads()
